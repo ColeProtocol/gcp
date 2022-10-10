305ef3251a5e9b407d8aa04ea28895b11c68f6a6
ColeProtocol <initiatethecoleprotocol@gmail.com>
2022-10-10 18:00:08 -0400

    no

---
+++ sterhello/sterhello/src/main/java/com/helloster/sterhello/SterhelloApplication.java
@@ -0,0 +1,15 @@
+package com.helloster.sterhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SterhelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SterhelloApplication.class, args);
+	}
+
+	
+
+}
---
+++ sterhello/sterhello/src/test/java/com/helloster/sterhello/SterhelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloster.sterhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SterhelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
